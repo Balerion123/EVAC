40cee6e434e59541dfa39d26b4ef4d7a87d42fd3
Balerion123 <adityaparmar9815@gmail.com>
2023-02-25 21:41:08 +0530

    connected routes and controllers

--- REST-server/app.js
+++ REST-server/app.js
@@ -8,6 +8,8 @@ const cors = require('cors');
 
 // const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -39,7 +41,8 @@ app.use(xss());
 
 // 2) ROUTES
 // USING GLOBAL HANDLER
-// app.use('/api/v1/users', userRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
